739df7f3b53aaa1e52bb1596f736315ade0e4485
NoelleDL <noelle.legrain@gmail.com>
2018-11-19 14:55:39 +0000

    first test and method for multiple of three

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,5 @@
+describe("FizzBuzz", function() {
+  it("returns Fizz when multiple of 3", function() {
+    expect(FizzBuzz(6)).toEqual("Fizz");
+  });
+});
---
+++ src/FizzBuzz.js
@@ -0,0 +1,4 @@
+function FizzBuzz(number) {
+  if (number % 3 == 0)
+    return "Fizz";
+}

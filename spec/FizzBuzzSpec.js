5f47d38eea8283f3d10cc20776fc04eb780c238c
NoelleDL <noelle.legrain@gmail.com>
2018-11-20 11:22:55 +0000

    fix first failing test

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,6 +1,8 @@
 describe("FizzBuzz", function() {
   it("returns Fizz when multiple of 3", function() {
-    expect(FizzBuzz(6)).toEqual("Fizz");
+    result = FizzBuzz(6);
+    result = result[5];
+    expect(result).toEqual("Fizz");
   });
 
   it("returns Buzz when multiple of 5", function() {

c00a89daa2ff8fa0f5a0cf9c5d322f0721827a23
NoelleDL <noelle.legrain@gmail.com>
2018-11-21 08:40:36 +0000

    refactor redundant line in tests

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,30 +1,32 @@
 describe("FizzBuzz", function() {
-  it("returns Fizz when multiple of 3", function() {
+
+  var result;
+
+  beforeEach(function() {
     result = FizzBuzz();
+  });
+
+  it("returns Fizz when multiple of 3", function() {
     result = result[5];
     expect(result).toEqual("Fizz");
   });
 
   it("returns Buzz when multiple of 5", function() {
-    result = FizzBuzz();
     result = result[9];
     expect(result).toEqual("Buzz");
   });
 
   it("returns FizzBuzz when multiple of 15", function() {
-    result = FizzBuzz();
     result = result[29];
     expect(result).toEqual("FizzBuzz");
   });
 
   it("returns the number when neither a multiple of 3, 5, nor 15", function() {
-    result = FizzBuzz();
     result = result[6];
     expect(result).toEqual(7);
   });
 
   it("returns an array of all numbers from 1 to 100", function() {
-    result = FizzBuzz();
     expect(result.length).toEqual(100);
   });
 });

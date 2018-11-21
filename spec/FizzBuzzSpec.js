dbebad77837e14e1d0a56585322137a99da440f4
NoelleDL <noelle.legrain@gmail.com>
2018-11-21 08:26:09 +0000

    remove argument from all tests

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,24 +1,24 @@
 describe("FizzBuzz", function() {
   it("returns Fizz when multiple of 3", function() {
-    result = FizzBuzz(6);
+    result = FizzBuzz();
     result = result[5];
     expect(result).toEqual("Fizz");
   });
 
   it("returns Buzz when multiple of 5", function() {
-    result = FizzBuzz(10);
+    result = FizzBuzz();
     result = result[9];
     expect(result).toEqual("Buzz");
   });
 
   it("returns FizzBuzz when multiple of 15", function() {
-    result = FizzBuzz(30);
+    result = FizzBuzz();
     result = result[29];
     expect(result).toEqual("FizzBuzz");
   });
 
   it("returns the number when neither a multiple of 3, 5, nor 15", function() {
-    result = FizzBuzz(7);
+    result = FizzBuzz();
     result = result[6];
     expect(result).toEqual(7);
   });

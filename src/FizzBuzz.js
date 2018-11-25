2abb64e36b1f119aca489e180a0faf3e31da01a9
NoelleDL <noelle.legrain@gmail.com>
2018-11-25 11:22:21 +0000

    refactor using ES6 syntax

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,19 +1,17 @@
-function FizzBuzz() {
-  var fizzBuzzer = { 3: "Fizz", 5: "Buzz"};
-  var numberArray = [];
+/*jshint esversion: 6*/
+
+const FizzBuzz = () => {
+  const fizzBuzzer = { 3: "Fizz", 5: "Buzz"};
+  let numberArray = [];
 
 
-  for (var number = 1; number <= 100; number++) {
-    var output = "";
-    for (var key in fizzBuzzer) {
-      if (number % key === 0) {
-        output += fizzBuzzer[key];
-      }
+  for (let number = 1; number <= 100; number++) {
+    let output = "";
+    for (let key in fizzBuzzer) {
+      if (number % key === 0) { output += fizzBuzzer[key]; }
     }
-    if (output === "") {
-      output = number;
-    }
+    if (output === "") { output = number; }
     numberArray.push(output);
   }
   return numberArray;
-}
+};

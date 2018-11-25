788d6e2972ee92e0a0fca2adf043a28a1f5e5f06
NoelleDL <noelle.legrain@gmail.com>
2018-11-25 10:21:43 +0000

    implement hash, all tests passing

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,21 +1,19 @@
 function FizzBuzz() {
+  var fizzBuzzer = { 3: "Fizz", 5: "Buzz"};
   var numberArray = [];
 
-  for (var count = 1; count <= 100; count++) {
-    switch (true) {
-      case count % 15 == 0:
-        numberArray.push("FizzBuzz");
-        break;
-      case count % 3 == 0:
-        numberArray.push("Fizz");
-        break;
-      case count % 5 == 0:
-        numberArray.push("Buzz");
-        break;
-      default:
-        numberArray.push(count);
-        break;
+
+  for (var number = 1; number <= 100; number++) {
+    var output = "";
+    for (var key in fizzBuzzer) {
+      if (number % key === 0) {
+        output += fizzBuzzer[key];
+      }
     }
+    if (output === "") {
+      output = number;
+    }
+    numberArray.push(output);
   }
   return numberArray;
 }
